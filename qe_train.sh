d9034eb049df303f1ccc6ddbc779b605bca9528c
hqWu <wuhuanqin@foxmail.com>
2019-03-01 11:31:16 +0800

    Update qe_train.sh

--- qe_train.sh
+++ qe_train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 datadir=./data/qe
 vocabdir=./data/vocab
 exp_modeldir=./saved_exp_model/best_BLEU  # or avg_best_BLEU if bilingual expert is trained with model averaging.
